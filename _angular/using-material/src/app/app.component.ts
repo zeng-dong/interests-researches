9c9a08adaa95c49c2e7840d2aaf002bfa354fabc
Dong Zeng <zeng-dong@hotmail.com>
2023-06-04 09:09:40 -0500

    converting to reactive

--- _angular/using-material/src/app/app.component.ts
+++ _angular/using-material/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.request = {};
+    this.request.person = { name: 'zeng', age: 5 };
+
+    console.log(this.request);
+  }
   title = 'using-material';
+
+  request: any;
 }
--- _angular/using-material/src/app/app.module.ts
+++ _angular/using-material/src/app/app.module.ts
@@ -3,20 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MaterialModule } from './shared/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCheckboxModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
