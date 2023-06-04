fa62a98a83a9cd6d1b1f6fda82f5dee0ba984032
Dong Zeng <zeng-dong@hotmail.com>
2023-06-04 09:21:30 -0500

    reactive

--- _angular/reactive-forms/src/app/app.module.ts
+++ _angular/reactive-forms/src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CustomerComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- _angular/reactive-forms/src/app/customers/customer.component.ts
+++ _angular/reactive-forms/src/app/customers/customer.component.ts
@@ -1,23 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
   customer = new Customer();
+  customerForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
   }
 
   save(customerForm: NgForm): void {
     console.log(customerForm.form);
     console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
+
+  saveReactive(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm));
+  }
+
+  populate(): void {
+    this.customerForm.setValue({
+      firstName: 'Michael',
+      lastName: 'Zeng',
+      email: 'mz@net.net',
+      sendCatalog: false,
+    });
   }
 }
