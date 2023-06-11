990e55eee482a849e0f0333b7bd948c6dcc71a1e
Dong Zeng <zeng-dong@hotmail.com>
2023-06-11 14:56:39 -0500

    tests

--- _angular/reactive-forms/src/app/heroes/heroes.component.spec.ts
+++ _angular/reactive-forms/src/app/heroes/heroes.component.spec.ts
@@ -1,5 +1,42 @@
-describe('a', () => {
-  it('should run', () => {
-    expect(1).toBeTruthy();
-  });
+import { of } from "rxjs";
+import { HeroService } from "../hero/hero.service";
+import { HeroesComponent } from "./heroes.component";
+
+describe('delete', () => {
+
+  let component: HeroesComponent;
+  let HEROES: any[];
+  let heroService: any;
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'SpiderMan', strength: 8 },
+      { id: 2, name: 'SpiderMan 2', strength: 24 },
+      { id: 3, name: 'SuperMan', strength: 55 },
+
+    ];
+
+    heroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    component = new HeroesComponent(heroService);
+  })
+
+    it('should remove the indicated hero from the heroes list', () => {
+      heroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+    });
+
+    it('should call deleteHero', () => {
+      heroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(heroService.deleteHero).toHaveBeenCalled();
+      expect(heroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+    });
 });
