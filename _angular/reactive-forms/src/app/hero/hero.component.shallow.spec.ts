e649c632b25b112a2faf18c191ff323064a6061e
Dong Zeng <zeng-dong@hotmail.com>
2023-06-16 19:18:41 -0500

    native and bebug el

--- _angular/reactive-forms/src/app/hero/hero.component.shallow.spec.ts
+++ _angular/reactive-forms/src/app/hero/hero.component.shallow.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroComponent } from './hero.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HeroComponent (shallow tests)', () => {
   let fixture: ComponentFixture<HeroComponent>;
@@ -8,6 +10,7 @@ describe('HeroComponent (shallow tests)', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     });
 
     fixture = TestBed.createComponent(HeroComponent);
@@ -18,5 +21,14 @@ describe('HeroComponent (shallow tests)', () => {
     component.hero = { id: 1, name: 'super man', strength: 3 };
 
     expect(component.hero.name).toEqual('super man');
+  });
+
+  it('should render the hero name in an anchor tag', () => {
+    component.hero = { id: 1, name: 'super man', strength: 3 };
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('super man');
+
+    expect(fixture.nativeElement.querySelector('a').textContent).toContain('super man');
   });
 });
