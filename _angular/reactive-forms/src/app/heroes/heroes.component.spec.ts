2c5b94e2535c47d9d8f985a873e94559e6fd3719
Dong Zeng <zeng-dong@hotmail.com>
2023-06-17 10:20:12 -0500

    shallow test

--- _angular/reactive-forms/src/app/heroes/heroes.component.spec.ts
+++ _angular/reactive-forms/src/app/heroes/heroes.component.spec.ts
@@ -1,6 +1,9 @@
 import { of } from 'rxjs';
 import { HeroService } from '../hero/hero.service';
 import { HeroesComponent } from './heroes.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Hero } from '../hero/hero';
 
 describe('delete', () => {
   let component: HeroesComponent;
@@ -36,5 +39,35 @@ describe('delete', () => {
 
     expect(heroService.deleteHero).toHaveBeenCalled();
     expect(heroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+  });
+});
+
+describe('HeroesComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let heroService: any;
+  let HEROES: Hero[];
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'SpiderMan', strength: 8 },
+      { id: 2, name: 'SpiderMan 2', strength: 24 },
+      { id: 3, name: 'SuperMan', strength: 55 },
+    ];
+    heroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    TestBed.configureTestingModule({
+      declarations: [HeroesComponent],
+      providers: [{ provide: HeroService, useValue: heroService }],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
+
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from the service', () => {
+    heroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
   });
 });
