2c7b4ec1a2ebc9b02324acf0f4e6f53e6b2e78d4
Dong Zeng <zeng-dong@hotmail.com>
2023-07-03 14:30:58 -0500

    spy

--- _angular/testing-angular-forms/src/app/services/calc.service.spec.ts
+++ _angular/testing-angular-forms/src/app/services/calc.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalcService } from './calc.service';
+import { SharedService } from './shared.service';
 
 describe('CalcService', () => {
   let service: CalcService;
 
   it('should multiply two numbers', () => {
-    service = new CalcService();
+    service = new CalcService(new SharedService());
     expect(service.mutiply(2, 3)).toBe(6);
+  });
+
+  it('should call the mySharedFunction function', () => {
+    //const sharedService = new SharedService();    // how can we avoid this? create a mock like this:
+
+    const sharedService = jasmine.createSpyObj('SharedService', [
+      'mySharedFunction',
+    ]);
+
+    //spyOn(sharedService, 'mySharedFunction'); // if we do not spy, we get an error: expect a spy, but got Function
+
+    //spyOn(sharedService, 'mySharedFunction').and.callThrough(); // then the function is called
+
+    const service = new CalcService(sharedService);
+    service.mutiply(2, 3);
+    expect(sharedService.mySharedFunction).toHaveBeenCalled();
   });
 });
--- _angular/testing-angular-forms/src/app/services/calc.service.ts
+++ _angular/testing-angular-forms/src/app/services/calc.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import { SharedService } from './shared.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalcService {
-  constructor() {}
+  constructor(private shareService: SharedService) {}
 
   mutiply(a: number, b: number): number {
+    this.shareService.mySharedFunction();
     return a * b;
   }
 }
