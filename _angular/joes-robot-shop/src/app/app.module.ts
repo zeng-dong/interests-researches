f835fafa6f6b156075c57db3bb517b40bfcbf40c
Dong Zeng <zeng-dong@hotmail.com>
2023-07-12 21:50:50 -0500

    prefix bot and first components

--- _angular/joes-robot-shop/src/app/app.module.ts
+++ _angular/joes-robot-shop/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule
