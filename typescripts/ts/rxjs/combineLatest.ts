ce6f981a36e82cf307fd04a12163124f99ae1760
Dong Zeng <zeng-dong@hotmail.com>
2023-10-16 20:18:41 -0500

    config

---
+++ typescripts/ts/rxjs/combineLatest.ts
@@ -0,0 +1,11 @@
+import { combineLatest, of } from "rxjs";
+
+const observable1 = of(1, 2, 3);
+const observable2 = of("a", "b", "c", "d");
+const observable3 = of(true, false, true, true, false);
+
+combineLatest([observable1, observable2, observable3]).subscribe({
+    next: (response: any) => {
+        console.log(response);
+    },
+});
