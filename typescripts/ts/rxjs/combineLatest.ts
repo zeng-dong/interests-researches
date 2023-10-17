8d157cea8d3402033218485dd4f8ef3e268a91b1
Dong Zeng <zeng-dong@hotmail.com>
2023-10-16 20:26:47 -0500

    looking into combinelatest

--- typescripts/ts/rxjs/combineLatest.ts
+++ typescripts/ts/rxjs/combineLatest.ts
@@ -4,6 +4,14 @@ const observable1 = of(1, 2, 3);
 const observable2 = of("a", "b", "c", "d");
 const observable3 = of(true, false, true, true, false);
 
+combineLatest([observable1, observable2]).subscribe({
+    next: (response: any) => {
+        console.log(response);
+    },
+});
+
+console.log("-----------------");
+
 combineLatest([observable1, observable2, observable3]).subscribe({
     next: (response: any) => {
         console.log(response);
