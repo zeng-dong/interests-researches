318f565e33f7a5497783c264ab9bd7f071d1eea9
Dong Zeng <zeng-dong@hotmail.com>
2023-11-08 20:21:43 -0600

    practice

---
+++ typescripts/ts/rxjs/takeWhile.ts
@@ -0,0 +1,8 @@
+// RxJS v6.4+
+import { of } from "rxjs";
+import { takeWhile, filter } from "rxjs/operators";
+
+const source$ = of(true, true, false, true, true, false);
+
+//source$.pipe(takeWhile((val) => val !== false, true))
+source$.pipe(takeWhile((val) => val !== false)).subscribe(console.log);
