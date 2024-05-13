c3c7f69eef0db3e5f27f6e06f575afdc23c7595b
Dong Zeng <zeng-dong@hotmail.com>
2024-05-12 19:04:09 -0500

    try setting up

--- typescripts/ts/rxjs/of.ts
+++ typescripts/ts/rxjs/of.ts
@@ -1,4 +1,4 @@
-import { Observable, debounceTime, delay, of } from "rxjs";
+import { Observable, debounceTime, delay, of, take, takeLast } from "rxjs";
 
 const source = of(function hello() {
     console.log("Hello world");
@@ -9,8 +9,25 @@ const subscribe = source.subscribe((val) => {
 });
 
 function apiGetTimes(): Observable<any> {
-    return of("morning", "afternoon").pipe(delay(0));
+    return of("morning", "afternoon").pipe(delay(20));
 }
 
 apiGetTimes().subscribe((x) => console.log("Good " + x));
 console.log("Good evening");
+
+let result: any = null;
+apiGetTimes()
+    .pipe(take(1))
+    .subscribe((x) => (result = x));
+
+try {
+    console.log(result.trim() + ", Sir");
+} catch (e) {
+    console.error(e);
+}
+
+setTimeout(() => {
+    console.log(result.trim() + ", Sir");
+}, 1);
+
+
