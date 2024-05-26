a98d26e8cf214ab4f6242e7423f3a400eb18a112
Dong Zeng <zeng-dong@hotmail.com>
2024-05-26 14:42:25 -0500

    styling wiht a mixin

--- _angular/angular-material-3/src/app/app.routes.ts
+++ _angular/angular-material-3/src/app/app.routes.ts
@@ -13,5 +13,33 @@ export const routes: Routes = [
         (c) => c.DashboardComponent
       ),
     title: 'Dashboard',
-  }
+  },
+  {
+    path: 'address',
+    loadComponent: () =>
+      import('./address-form/address-form.component').then(
+        (c) => c.AddressFormComponent
+      ),
+    title: 'Address',
+  },
+  {
+    path: 'table',
+    loadComponent: () =>
+      import('./table/table.component').then((c) => c.TableComponent),
+    title: 'Table',
+  },
+  {
+    path: 'tree',
+    loadComponent: () =>
+      import('./tree/tree.component').then((c) => c.TreeComponent),
+    title: 'Tree',
+  },
+  {
+    path: 'drag-drop',
+    loadComponent: () =>
+      import('./drag-drop/drag-drop.component').then(
+        (c) => c.DragDropComponent
+      ),
+    title: 'Drag-Drop',
+  },
 ];
