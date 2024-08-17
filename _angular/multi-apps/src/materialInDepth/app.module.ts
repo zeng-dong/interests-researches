f3ae8b969794d8fd12cd29c019b08e68a68a3169
Dong Zeng <zeng-dong@hotmail.com>
2024-08-17 11:36:23 -0500

    move material modules to shared

--- _angular/multi-apps/src/materialInDepth/app.module.ts
+++ _angular/multi-apps/src/materialInDepth/app.module.ts
@@ -4,40 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import {
     provideHttpClient,
     withInterceptorsFromDi,
 } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatTreeModule } from '@angular/material/tree';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MaterialModule } from '../shared/material-module';
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, AboutComponent],
@@ -45,34 +19,9 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MatMenuModule,
-        MatButtonModule,
-        MatIconModule,
-        MatCardModule,
-        MatTabsModule,
-        MatSidenavModule,
-        MatListModule,
-        MatToolbarModule,
-        MatTooltipModule,
-        MatInputModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatProgressSpinnerModule,
-        MatStepperModule,
-        MatDialogModule,
         AppRoutingModule,
-        MatSelectModule,
-        MatCheckboxModule,
-        DragDropModule,
-        MatRadioModule,
-        MatSliderModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
+        MaterialModule,
         ReactiveFormsModule,
-        MatGridListModule,
-        MatTreeModule,
-        ScrollingModule,
     ],
     providers: [provideHttpClient(withInterceptorsFromDi())],
 })
