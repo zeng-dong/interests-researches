71b7eb1feac178a4b754155306b7903b5238f0e3
Dong Zeng <zeng-dong@hotmail.com>
2024-08-17 09:59:35 -0500

    added material, material in depth module, bootstrap from this module

--- _angular/multi-apps/src/main.ts
+++ _angular/multi-apps/src/main.ts
@@ -2,15 +2,24 @@ import { bootstrapApplication } from '@angular/platform-browser';
 // import { appConfig } from './app/app.config';
 // import { AppComponent } from './app/app.component';
 
-import { appConfig } from './app3/app.config';
-import { AppComponent } from './app3/app.component';
+//// bootstrap module: material module
+//import { appConfig } from './materialInDepth/app.config';
+import { AppModule } from './materialInDepth/app.module';
+//import { AppComponent } from './materialInDepth/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.log(err));
 
-import { app2Config } from './appNumberTwo/app2.config';
-import { App2Component } from './appNumberTwo/app2.component';
+// import { appConfig } from './app3/app.config';
+// import { AppComponent } from './app3/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+// import { app2Config } from './appNumberTwo/app2.config';
+// import { App2Component } from './appNumberTwo/app2.component';
+
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err)
+// );
 
 // bootstrapApplication(App2Component, app2Config).catch((err) =>
 //   console.error(err)
---
+++ _angular/multi-apps/src/materialInDepth/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
