28d8f60bc4c4e81afe4006360c2493b102d7a913
Dong Zeng <zeng-dong@hotmail.com>
2024-08-29 11:12:48 -0500

    added bootstrap

--- _angular/multi-apps/src/tdf/app.module.ts
+++ _angular/multi-apps/src/tdf/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { UserSettingsComponent } from "./user-settings/user-settings.component";
 
 @NgModule({
     declarations: [AppComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, CommonModule],
+    imports: [BrowserModule, CommonModule, UserSettingsComponent],
 })
 export class AppModule {}
