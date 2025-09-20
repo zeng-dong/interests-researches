03cfd600939cab3f36ddab64d1411aaf0c7994ba
Dong Zeng <zeng-dong@hotmail.com>
2025-09-20 10:33:09 -0500

    adding hello signals and rxjs

---
+++ ts-2025/hello.ts
@@ -0,0 +1,13 @@
+import { signal } from "@angular/core";
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
+
+console.log("hello world");
+
+const s = signal<string>("hello signals");
+
+console.log(s());
+
+of(1, 2, 3)
+	.pipe(map((x) => x * x))
+	.subscribe((v) => console.log(`value: ${v}`));
